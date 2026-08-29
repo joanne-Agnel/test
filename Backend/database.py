4de6f60b14874c2acee6d2eaffd64c6d3d02c98e
joanne-Agnel <joanne.agnel@gmail.com>
2026-08-29 14:17:00 +0100

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 # Reading a JavaScript file
 with open('PerfectDay.js', 'r') as js_file:
    content = js_file.read()
 # Print or process the content
 print(content)
+=======
+import json
+
+try:
+    with open("data.json", "r") as file:
+        data = json.load(file)
+    print("File data =", data)
+except json.JSONDecodeError:
+    print("Error: Failed to decode JSON from the file.")
+
+>>>>>>> Stashed changes
