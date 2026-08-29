d842c1944c9db1c940784a218f6fad4adf0d3270
chloetam816 <88521690+chloetam816@users.noreply.github.com>
2026-08-29 19:28:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import uvicorn
 import json
 from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+
+#app.add_middleware(
+ #   CORSMiddleware,
+  #  allow_origins=["*"],
+   # allow_credentials=True,
+    #allow_methods=["*"],
+    #allow_headers=["*"],
+#)
 
 @app.get("/")
 @app.head("/")
