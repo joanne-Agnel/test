44d7b7101e1ce9b0626ff6a68b5b1ccd44d7fab0
chloetam816 <88521690+chloetam816@users.noreply.github.com>
2026-08-29 22:10:17 +0800

    python

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ from fastapi import FastAPI, Depends
 import uvicorn
 import json
 from fastapi.middleware.cors import CORSMiddleware
+from Backend.database import connect_db, disconnect_db
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connect_db()
+    yield
+    await disconnect_db()
 
-#app.add_middleware(
- #   CORSMiddleware,
-  #  allow_origins=["*"],
-   # allow_credentials=True,
-    #allow_methods=["*"],
-    #allow_headers=["*"],
-#)
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 @app.head("/")
 def home():
     return {"message": "backend is running"}
+
